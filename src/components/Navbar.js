187be2a137e46186e339950bfa8f9240886b8bc4
Ankush Dutta <ankush.ad050203@gmail.com>
2023-01-10 23:19:17 +0530

    pages and components initialized

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row w-[100vw] h-[fit-content] p-[20px] justify-between'>
+            <label className='font-montserrat text-[50px] text-purple-900 font-light p-2 text-center text-2xl rounded-full' >
+                Flow
+            </label>
+        </div>
+  )
+}
+
+export default Navbar
