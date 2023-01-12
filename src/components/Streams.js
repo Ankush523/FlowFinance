37611d8ecd941fde22f24e793cebea2df38b29d6
Ankush Dutta <ankush.ad050203@gmail.com>
2023-01-12 22:09:09 +0530

    streams page complete and dashboard minor changes

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -2,16 +2,27 @@ import React from 'react'
 
 const Streams = () => {
   return (
-    <div className='bg-white2'>
-        <div className='w-[80%] h-[fit-content] bg-white1 shadow-xl rounded-xl'>
-            <div className='flex flex-col'>
-                <label>My Streams</label>
-                <div className='flex flex-row justify-between'>
+    <div className='bg-white2 py-[100px] flex flex-row justify-center items-center'>
+        <div className='w-[60%] h-[fit-content] bg-white1 shadow-2xl rounded-2xl '>
+            <div className='flex flex-col px-[20px] space-y-4 py-[20px]'>
+                <label className='text-left bg-green1/10 w-[fit-content] p-[10px] rounded-xl text-green1 font-poppins font-semibold text-xl'>My Streams</label>
+                <div className='flex flex-row justify-between px-[10px] font-poppins text-black1/70'>
                     <label>Pool</label>
                     <label>Start Date</label>
                     <label>Balances</label>
                 </div>
-                
+                <div className='flex flex-col space-y-4 '>
+                    <div className='w-full p-4 text-transparent bg-white2 rounded-2xl animate-pulse'>
+                        -
+                    </div>
+                    <div className='w-full p-4 text-transparent bg-white2 rounded-2xl animate-pulse'>
+                        -
+                    </div>
+                    <div className='w-full p-4 text-transparent bg-white2 rounded-2xl animate-pulse'>
+                        -
+                    </div>
+                </div>
+                <p className='text-left px-[10px] font-poppins text-black1/70'>You currently don't have any streams</p>
             </div>
         </div>
     </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -16,50 +16,60 @@ const Dashboard = () => {
             <label className="font-poppins font-medium text-green1  text-4xl">flow</label>
           </div>
 
-          <a href="https://github.com/Ankush523/FlowFinance">
-            <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] bg-green3 py-[8px] hover:bg-green1 mt-[40px]">
-              <img src={wallet} alt="wallet" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
-              <p className="font-poppins text-sm font-medium text-white1">
-                Connect Wallet
-              </p>
-            </div>
-          </a>
+          <button>
+            <a href="https://github.com/Ankush523/FlowFinance">
+                <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] bg-green3 py-[8px] hover:bg-green1 mt-[40px]">
+                <img src={wallet} alt="wallet" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
+                <p className="font-poppins text-sm font-medium text-white1">
+                    Connect Wallet
+                </p>
+                </div>
+            </a>
+          </button>
+          
+          <button>
+            <a href="https://github.com/Ankush523/FlowFinance">
+                <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[20px]">
+                <img src={dashboard} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
+                <p className="font-poppins text-sm font-medium text-green3">
+                    Dashboard
+                </p>
+                </div>
+            </a>
+          </button>
+          
+          <button>
+            <a href="https://github.com/Ankush523/FlowFinance">
+                <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[10px]">
+                <img src={stream} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
+                <p className="font-poppins text-sm font-medium text-green3">
+                    My Streams
+                </p>
+                </div>
+            </a>
+          </button>
+          
+          <button>
+            <a href="https://github.com/Ankush523/FlowFinance">
+                <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[10px]">
+                <img src={lend} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
+                <p className="font-poppins text-sm font-medium text-green3">
+                    Lend
+                </p>
+                </div>
+            </a>
+          </button>
 
-          <a href="https://github.com/Ankush523/FlowFinance">
-            <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[20px]">
-              <img src={dashboard} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
-              <p className="font-poppins text-sm font-medium text-green3">
-                Dashboard
-              </p>
-            </div>
-          </a>
-
-          <a href="/streams">
-            <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[10px]">
-              <img src={stream} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
-              <p className="font-poppins text-sm font-medium text-green3">
-                My Streams
-              </p>
-            </div>
-          </a>
-
-          <a href="https://github.com/Ankush523/FlowFinance">
-            <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[10px]">
-              <img src={lend} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
-              <p className="font-poppins text-sm font-medium text-green3">
-                Lend
-              </p>
-            </div>
-          </a>
-
-          <a href="https://github.com/Ankush523/FlowFinance">
-            <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[10px]">
-              <img src={borrow} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
-              <p className="font-poppins text-sm font-medium text-green3">
-                Borrow
-              </p>
-            </div>
-          </a>
+          <button>
+            <a href="https://github.com/Ankush523/FlowFinance">
+                <div className="flex flex-row space-x-2 items-center pl-[8px] rounded-xl mx-[10px] hover:bg-green1/10 py-[8px]  mt-[10px]">
+                <img src={borrow} alt="dashboard" className="h-[35px] p-[5px] bg-white1/20 rounded-md" />
+                <p className="font-poppins text-sm font-medium text-green3">
+                    Borrow
+                </p>
+                </div>
+            </a>
+          </button>
         </div>
       </div>
 
