d3ff63e151822b8f8dfd0ba2362ff80901009259
Ankush Dutta <ankush.ad050203@gmail.com>
2023-01-15 01:01:30 +0530

    baisc structure of borrow page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import Streams from './components/Streams';
+import Borrow from './components/Borrow';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/streams' element={<Streams/>}/>
+          <Route path='/borrow' element={<Borrow/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Borrow.js
+++ src/components/Borrow.js
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Borrow = () => {
   return (
-    <div className='bg-white2 text-4xl'>Borrow</div>
+    <div className='bg-white2 py-[100px] flex flex-row justify-center items-center'>
+        <div className='w-[45%] h-[fit-content] bg-white1 shadow-xl rounded-2xl'>
+            <div className='flex flex-col px-[30px] space-y-4 py-[30px]'>
+                <label className='text-left bg-green1/10 w-[fit-content] p-[10px] rounded-xl text-green1 font-poppins font-semibold text-xl'>Borrow Order</label>
+                <div className='flex flex-row justify-between'>
+                    <label>Fixed APY</label>
+                    <label>3.982%</label>
+                </div>
+
+                <div className='flex flex-col'>
+                    <label>How much do you want to borrow?</label>
+                    <div>
+                        <input className='' placeholder='0.0000000...'/>
+
+                    </div>
+                </div>
+
+                <div className='flex flex-col'>
+                    <label>Specify collateral to deposit</label>
+                    <div>
+                        <input className='' placeholder='0.0000000...'/>
+
+                    </div>
+                </div>
+
+                <button>Confirm and Submit</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
